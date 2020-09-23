74a9f085190b248b4a75b3819034fecfc1033c98
Alexandre Tertuliano <38267460+AlexandreTertuliano@users.noreply.github.com>
2020-09-22 22:37:24 -0300

    Prova

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>AGENCIAS CAIXA</title>
+</head>
+
+<body>
+    <div id="page">
+		
+        <div id="header">
+        	<div id="headerTitle">Agencias Caixa</div>
+          
+            
+        </div>
+        <div id="bar">
+            <a href="atendimento.php">ACESSAR</a>
+            
+            
+      </div>
+<style>
+  html, body {
+text-align: center;
+}
+p {text-align: left;}
+body {
+	margin: 0;
+	padding: 0;
+	background: #697281;
+	text-align: left;
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	font-size: 13px;
+	color: #F1F5F8;
+	background-color:blue;
+}
+*
+{
+  margin: 0 auto 0 auto;
+ text-align:left;}
+#header
+{
+background-color:blue;
+height:111px;
+width:100%;
+}
+#headerTitle
+{
+position:relative;
+left:30px;
+top:30px;
+color:orangered;
+text-align:left;
+font-size:34px;
+font-weight:bold;
+font-family:Arial, Helvetica, sans-serif;
+}
+#headerSubText
+{
+position:relative;
+left:33px;
+top:30px;
+color:#679159;
+text-align:left;
+font-size:16px;
+font-weight:bold;
+font-family:Arial, Helvetica, sans-serif;
+}
+#bar
+{
+background-color:orangered;
+height:55px;
+width:100%;
+line-height:55px;
+padding-left:20px;
+padding-right:20px;
+}
+#bar a
+{
+font-size:13px;
+font-weight:bold;
+color:#FBFDFB;
+margin-left:28px;
+margin-right:28px;
+}
+#bar a:hover
+{
+color:#C7E0C2;
+}
+.contentTitle
+{
+width:676px;
+height:37px;
+margin-top:20px;
+margin-left:25px;
+margin-right:25px;
+}
+.contentTitle h1
+{
+margin-left:21px;
+padding-top:8px;
+font-size:17px;
+font-weight:bold;
+color:#071027;
+}
+.comments
+{
+font-size:11px;
+font-style:italic;
+}
+.contentText
+{
+width:680px;
+padding-left:30px;
+padding-right:30px;
+font-size:13px;
+color:#030712;
+line-height:30px;
+}
+a
+{
+text-decoration:none;
+color:#152413;
+}
+a:hover
+{
+color:#F1F5F8;
+}
+  
+</style>
